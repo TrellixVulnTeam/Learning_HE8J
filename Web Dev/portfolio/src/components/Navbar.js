80c39b1717f83c395af4923903556e45cb32c809
Tai <kktai1512@outlook.com>
2022-07-12 00:08:49 +0800

    add padding

--- Web Dev/portfolio/src/components/Navbar.js
+++ Web Dev/portfolio/src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between p-4 items-center text-white font-semibold">
-      <div>LOGO</div>
-      <div className="flex gap-10 items-center">
-        <div> Home </div>
-        <div> Skills </div>
-        <div> Projects </div>
-        <div className="border px-3 py-2"> Let's Connect </div>
-      </div>
+    <div className="flex justify-between p-5 items-center text-white font-semibold">
+      <h1 className="text-3xl">LOGO</h1>
+      <ul className="flex gap-10 items-center">
+        <li className="hover:cursor-pointer"> Home </li>
+        <li className="hover:cursor-pointer"> Skills </li>
+        <li className="hover:cursor-pointer"> Projects </li>
+        <li className="border px-3 py-2 hover:cursor-pointer"> Let's Connect </li>
+      </ul>
     </div>
   )
 }
