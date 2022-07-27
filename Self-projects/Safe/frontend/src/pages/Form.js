a256547eac33cdefbd13a81a89957439812489d2
Tai <kktai1512@outlook.com>
2022-07-28 01:01:10 +0800

    know the importance of redux

--- Self-projects/Safe/frontend/src/pages/Form.js
+++ Self-projects/Safe/frontend/src/pages/Form.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components/macro"
 import { motion } from "framer-motion"
 
-import { Fab, Zoom } from "@mui/material"
+import { ButtonBase, Fab, Zoom } from "@mui/material"
 import CheckIcon from "@mui/icons-material/Check"
 import { useNavigate } from "react-router-dom"
-import { css } from "styled-components"
+
+import "./Dropdown.css"
+import { OptionUnstyled, SelectUnstyled } from "@mui/base"
+import "../components/Transactions/Ripple.css"
 
 const Container = styled.div`
   display: flex;
@@ -50,6 +53,33 @@ const InputContainer = styled.div`
     color: black;
   }
 `
+
+const DropDownContainer = styled.div`
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  border: none;
+  margin-bottom: 30px;
+  background-color: lightgrey;
+  color: black;
+  padding: 5px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  > * {
+    color: black;
+  }
+`
+const DropdownItem = styled.div`
+  height: 40px;
+  font-size: 20px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  /* justify-content: left; */
+  color: black;
+`
+
 const Image = styled.div`
   width: 107px;
   height: 107px;
@@ -87,8 +117,76 @@ const BasicDiv = styled.div`
   font-size: 20px;
   font-weight: 700;
 `
+
+const Input = ({ placeholder, value, setValue, onClick }) => {
+  return (
+    <InputContainer onClick={onClick}>
+      <SmallInput value={value} placeholder={placeholder} />
+    </InputContainer>
+  )
+}
+const MenuItem = ({ value, onClick, setValue }) => {
+  return (
+    <ButtonBase
+      onClick={() => {
+        setValue(value)
+        onClick()
+      }}
+      style={{ justifyContent: "flex-start" }}
+    >
+      <DropdownItem>{value}</DropdownItem>
+    </ButtonBase>
+  )
+}
+
+const Dropdown = ({ choices, onClick, setValue }) => {
+  return (
+    <DropDownContainer>
+      {choices.map((choice) => (
+        <MenuItem onClick={onClick} value={choice} setValue={setValue} />
+      ))}
+    </DropDownContainer>
+  )
+}
+
+const choices = ["DBS", "HSBC", "Maybank"]
+
+const InnerForm = () => {
+  const [visible, setVisible] = useState(true)
+  const [value, setValue] = useState("")
+  const [CRDB, setCRDB] = useState("CR")
+  const [amount, setAmount] = useState(0)
+
+  return (
+    <>
+      <InputContainer>
+        <SmallInput onChange={(e) => setAmount(e.target.value)} placeholder="$12,000" />
+        <BasicDiv onClick={() => setCRDB(CRDB === "CR" ? "DB" : "CR")}>{CRDB}</BasicDiv>
+      </InputContainer>
+      <Input
+        value={value}
+        placeholder="DBS"
+        onClick={() => {
+          console.log(visible)
+          setVisible(!visible)
+        }}
+      />
+      {visible && (
+        <Dropdown
+          choices={choices}
+          onClick={() => {
+            setVisible(!visible)
+          }}
+          setValue={setValue}
+        />
+      )}
+      <Input placeholder="Description" />
+    </>
+  )
+}
 const Form = () => {
   const navigate = useNavigate()
+  const [name, setName] = useState(" ")
   const onClick = () => {
     navigate("/")
   }
@@ -101,20 +199,13 @@ const Form = () => {
               <Title>
                 <Image />
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                  <TitleInput placeholder="Caifan" />
+                  <TitleInput onChange={(e) => setName(e.target.value)} placeholder="Caifan" />
                   <div style={{ fontSize: "20px", fontWeight: 400, color: "black", fontFamily: "Inter" }}>Food</div>
                 </div>
               </Title>
-              <InputContainer>
-                <SmallInput placeholder="$12,000" />
-                <BasicDiv>CR</BasicDiv>
-              </InputContainer>
-              <InputContainer>
-                <SmallInput placeholder="DBS" />
-              </InputContainer>
-              <InputContainer>
-                <SmallInput placeholder="Receipt" />{" "}
-              </InputContainer>
+
+              {/* Inner Form */}
+              <InnerForm />
             </Background>
           </InnerContainer>
         </motion.div>
